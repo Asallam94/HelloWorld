048ad326ab522b8eb7953d2a9720056a134aa58c
Amani <asallam@umich.edu>
2021-10-06 02:27:42 -0400

    Change name and loop 14 times

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
-public class Hello {
+public class Hello20 {
 	public static void main(String[] args) {
 
-		for(int i=1;i<=13;i++) {
+		for(int i=1;i<=14;i++) {
 
-			System.out.println("Hello World..."+i);
+			System.out.println("Hello World..."+i)
 		}
 	}
 }
